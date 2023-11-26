64070b02b9b81d073dac4ce2f40c4bdd498064c8
ST32-217\Student <ryszard.kuczowic@chorzow.merito.pl>
2023-11-26 15:43:11 +0100

    Dodaj pliki projektów.

---
+++ Trojkat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class Trojkat 
+{
+    static void Main(string[] args)
+    {
+        double a, b, c;
+        Console.Write("Wpisz wartosc przyprostokatnej A: ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("Wpisz wartosc przyprostokatnej B: ");
+        b = double.Parse(Console.ReadLine());
+        c = (a * a + b * b);
+        Console.WriteLine("Przeciwprostokatna C : {0}", c);
+        c = Math.Sqrt(a * a + b * b);
+        Console.WriteLine("Pierwiastek : {0}", c);
+        Console.ReadLine();
+
+    }
+}
